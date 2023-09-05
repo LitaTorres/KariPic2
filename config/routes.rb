acd2840bb9f2e2cec362df20f24977be1f0da52e
Leslie Torres <Leslietorresc@gmail.com>
2023-09-05 18:56:56 -0300

    commit 2 scaffold publications

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :publications
   devise_for :users
   get 'homes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
    root "homes#index"
+   get "/publications", to: "publications#index", as: "user_root"
 end
